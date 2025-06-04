40057ec3766717de74ae7e88ca237590ff37d8ba
Ruth <conwayjazz95@gmail.com>
2025-06-04 14:47:35 -0400

    update 6/4

---
+++ analyses/bayes_summary.R
@@ -0,0 +1,11 @@
+rm(list=ls())
+library(here)
+library(tidyverse)
+load(here("analyses/stan/m14/m14_summary.RData"))
+options(pillar.sigfig = 3)
+t <- as_tibble(fit_summary$summary) %>%
+  mutate(variable=rownames(fit_summary$summary)) %>%
+  relocate(variable,.before=everything()) %>%
+  filter(str_detect(variable,"\\[",negate=T))
+t[1:13,] %>%
+  select(-se_mean)
